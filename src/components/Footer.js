1359778af406619156a00ffb154c949dfbafffab
brystmar <brystmar@users.noreply.github.com>
2021-10-04 19:21:14 -0500

    Footer is done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,43 +2,26 @@ import React from "react";
 
 export default function Footer() {
     return <footer className="footer-container">
-        <div className="tagline">
-            {`Another surprisingly-not-food-centric app \nby `}
+        <span className="footer-item">{`A webapp that, surprisingly, isn't about food \nby `}</span>
+        <span className="footer-item">
             <a
                 href="http://thomasberg.me/"
                 target="_blank"
                 rel="noopener noreferrer"
             >Thomas Berg</a>
-        </div>
-
-        <div className="links">
-            <span className="logo-container">
-                <a
-                    href="https://github.com/brystmar/greeting-cards-ui/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <img
-                        src="./logos/github.svg"
-                        alt="GitHub logo"
-                        className="logo"
-                    />
-                </a>
-            </span>
-
-            <span className="logo-container">
-                <a
-                    href="http://breadsheet.com/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <img
-                        src="./logos/bread-loaf-long.png"
-                        alt="Spinning rustic baguette"
-                        className="logo"
-                    />
-                </a>
-            </span>
-        </div>
+        </span>
+        <span className="footer-item">
+            <a
+                href="https://github.com/brystmar/"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <img
+                    src="./logos/github.svg"
+                    alt="GitHub logo"
+                    className="logo"
+                />
+            </a>
+        </span>
     </footer>
 }
