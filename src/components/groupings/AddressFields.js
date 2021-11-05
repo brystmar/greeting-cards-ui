c80f484ae8763b52a4c754e8b51764b9939f15a5
Thomas <brystmar@users.noreply.github.com>
2021-11-05 13:05:53 -0700

    Adjusted how addresses are displayed, including support for full_address

--- src/components/groupings/AddressFields.js
+++ src/components/groupings/AddressFields.js
@@ -5,76 +5,119 @@ export default function AddressFields(props) {
     return (
         <div className="address-fields">
             <div className="label-input-container">
-                <label htmlFor="address-street-1" className="label-input">Street 1:</label>
+                <label
+                    htmlFor={`address-line-1-${props.index}`}
+                    className="label-input"
+                >Line 1:</label>
+
                 <input
                     type="text"
-                    id={`address-street-1-${props.index}`}
-                    name="address-street-1"
-                    value={props.line_1 ? props.line_1 : ""}
-                    className="input-text"
+                    id={`address-line-1-${props.index}`}
+                    name="address-line-1"
+                    value={props.line_1 || ""}
+                    className="input-text input-address-street"
                     disabled={true}
                 />
             </div>
 
             <div className="label-input-container">
-                <label htmlFor="address-street-2" className="label-input">Street 2:</label>
+                <label
+                    htmlFor={`address-line-2-${props.index}`}
+                    className="label-input"
+                >Line 2:</label>
+
                 <input
                     type="text"
-                    id={`address-street-2-${props.index}`}
-                    name="address-street-2"
-                    value={props.line_2 ? props.line_2 : ""}
-                    className="input-text"
+                    id={`address-line-2-${props.index}`}
+                    name="address-line-2"
+                    value={props.line_2 || ""}
+                    className="input-text input-address-street"
                     disabled={true}
                 />
             </div>
 
             <div className="label-input-container">
-                <label htmlFor="address-city" className="label-input">City:</label>
+                <label
+                    htmlFor={`address-city-${props.index}`}
+                    className="label-input"
+                >City:</label>
+
                 <input
                     type="text"
                     id={`address-city-${props.index}`}
                     name="address-city"
-                    value={props.city ? props.city : ""}
-                    className="input-text"
+                    value={props.city || ""}
+                    className="input-text input-address-city"
                     disabled={true}
                 />
             </div>
 
             <div className="label-input-container">
-                <label htmlFor="address-state" className="label-input">State:</label>
+                <label
+                    htmlFor={`address-state-${props.index}`}
+                    className="label-input"
+                >State:</label>
+
                 <input
                     type="text"
                     id={`address-state-${props.index}`}
                     name="address-state"
-                    value={props.state ? props.state : ""}
-                    className="input-text"
+                    value={props.state || ""}
+                    className="input-text input-address-state"
                     disabled={true}
                 />
             </div>
 
             <div className="label-input-container">
-                <label htmlFor="address-zip" className="label-input">Zip:</label>
+                <label
+                    htmlFor={`address-zip-${props.index}`}
+                    className="label-input"
+                >Zip:</label>
+
                 <input
                     type="text"
                     id={`address-zip-${props.index}`}
                     name="address-zip"
-                    value={props.zip ? props.zip : ""}
-                    className="input-text"
+                    value={props.zip || ""}
+                    className="input-text input-address-zip"
                     disabled={true}
                 />
             </div>
 
             <div className="label-input-container">
-                <label htmlFor="address-country" className="label-input">Country:</label>
+                <label
+                    htmlFor={`address-country-${props.index}`}
+                    className="label-input"
+                >Country:</label>
+
                 <input
                     type="text"
                     id={`address-country-${props.index}`}
                     name="address-country"
-                    value={props.country ? props.country : ""}
-                    className="input-text"
+                    value={props.country || ""}
+                    className="input-text input-address-country"
                     disabled={true}
                 />
             </div>
+
+            {props.full_address ?
+                // Only display the full address when it's used, typically for non-US addresses
+                <div className="label-input-container">
+                    <label
+                        htmlFor={`address-full-${props.index}`}
+                        className="label-input"
+                    >Full Address:</label>
+
+                    <textarea
+                        id={`address-full-${props.index}`}
+                        name="address-full"
+                        value={props.full_address || ""}
+                        className="input-textarea input-address-full"
+                        disabled={true}
+                    />
+                </div>
+                : null}
+
         </div>
     )
 }
